dad2dfb8ecdcab7a1105a0cb5c91a5e32b14eef6
Stefanos Carlström <stefanos.carlstrom@gmail.com>
2019-04-01 13:31:10 +0200

    Added preliminary plotting support

--- src/HDF5Browse.jl
+++ src/HDF5Browse.jl
@@ -2,10 +2,14 @@ module HDF5Browse
 
 using HDF5
 using PrettyTables
+using UnicodePlots
+using LinearAlgebra
+using Statistics
 
 function print_block(fun::Function, out=stdout; s="╭", m="│", e="╰")
     io = IOBuffer()
-    fun(io)
+    ds = displaysize(out)
+    fun(IOContext(io, :displaysize=>(ds[1],ds[2]-10)))
     output = String(take!(io))
     isempty(output) && return
     data = split(output, "\n")
@@ -41,6 +45,52 @@ function hdf5browse(x::Union{HDF5File, HDF5Group}, io::IO=stdout)
     end
 end
 
+getPlot(x, y, io::IO; canvas::Type = BrailleCanvas,
+        height=cld(displaysize(io)[1],3),
+        width=displaysize(io)[2]-15,
+        kwargs...) =
+    Plot(x, y, canvas; height=height, width=width, kwargs...)
+
+function autolineplot!(plt::Plot, data::AbstractVector{<:Real}, args...; kwargs...)
+    # v = if all(abs.(data .- 1.0) .< 0.05)
+    #     data .- 1.0
+    # else
+    #     data
+    # end
+    v = data
+    x = 1:length(v)
+    vv = abs.(v)
+    # if dot(x,vv)/norm(vv) < 0.3length(v)
+    #     x = log.(x)
+    # end
+    lineplot!(plt, x, v, args...; kwargs...)
+    plt
+end
+
+autolineplot(data::AbstractVector{<:Real}, io::IO, args...; kwargs...) =
+    autolineplot!(getPlot(axes(data,1), data, io), data, args...; kwargs...)
+
+function autolineplot!(plt::Plot, data::AbstractMatrix{<:Real}, args...; kwargs...)
+    m,n = size(data)
+    if m > n
+        for j = 1:n
+            autolineplot!(plt, view(data, :, j), args...; kwargs...)
+        end
+    else
+        for i = 1:m
+            autolineplot!(plt, view(data, i, :), args...; kwargs...)
+        end
+    end
+    plt
+end
+
+function autolineplot(data::AbstractMatrix{<:Real}, io::IO, args...;
+                      canvas::Type = BrailleCanvas, kwargs...)
+    m,n = size(data)
+    plt = m > n ? getPlot(1:m, view(data, :, 1), io) : getPlot(1:n, view(data, 1, :), io)
+    autolineplot!(plt, data, args...; kwargs...)
+end
+
 function hdf5browse(x::HDF5Dataset, io::IO=stdout)
     println(io, x)
     hdf5printattrs(x, io)
@@ -48,6 +98,12 @@ function hdf5browse(x::HDF5Dataset, io::IO=stdout)
     println(io, eltype(data), " ", size(data))
     show(IOContext(io, :limit=>true), data)
     println(io)
+    if eltype(data) <: Real
+        println(io, displaysize(io))
+        plt = autolineplot(data, io)
+        show(IOContext(io, :color=>true), plt)
+        println()
+    end
 end
 
 hdf5browse(filename::AbstractString) = h5open(hdf5browse, filename, "r")
